c4d78fb39501d5ca7561692fd1244cf01382687b
Quang Vu <quangvu2081810@gmail.com>
2024-01-25 18:08:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// var number_cart = 0;
+// function themvaogiohang() {
+//     console.log("hello");
+//     document.getElementById("number").innerHTML = ++number_cart;
+    
+
+// }
+
+var number_cart = 0;
+function themvaogiohang() {
+    console.log("hello");
+    document.getElementById("number").innerHTML = ++number_cart;
+    
+
+}
